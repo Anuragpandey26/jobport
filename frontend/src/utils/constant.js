034bcf254398e5213c643a373f38724d8d1e412a
Anuragpandey26 <anuragkumarpandey2001@gmail.com>
2025-05-28 09:33:54 +0530

    fifth commit

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="https://jobportbackend.onrender.com/";
-export const JOB_API_END_POINT="https://jobportbackend.onrender.com/";
-export const APPLICATION_API_END_POINT="https://jobportbackend.onrender.com/";
-export const COMPANY_API_END_POINT="https://jobportbackend.onrender.com/";
+export const USER_API_END_POINT="https://jobportbackend.onrender.com";
+export const JOB_API_END_POINT="https://jobportbackend.onrender.com";
+export const APPLICATION_API_END_POINT="https://jobportbackend.onrender.com";
+export const COMPANY_API_END_POINT="https://jobportbackend.onrender.com";
